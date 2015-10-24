5c8adde376e3e717afa64dfd58db9dfa017e5f9b
Hayden McAfee <hayden@outlook.com>
2015-10-23 21:50:29 -0700

    Cleaned up Boink to be a real library instead of just an experiment in an ASP project.

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        ts: {
+            default: {
+                src: ["src/**/*.ts"],
+                options: {
+                    declaration: true
+                },
+                out: 'out/boink.js'
+            }
+        },
+        tslint: {
+            options: {
+                configuration: grunt.file.readJSON("tslint.json")
+            },
+            files: {
+                src: ['src/**/*.ts']
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'out/boink.min.js': ['out/boink.js']
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-tslint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    grunt.registerTask('default', ['tslint', 'ts', 'uglify']);
+};
---
+++ src/Data/TodoDataModel.ts
@@ -0,0 +1,20 @@
+﻿class TodoDataModel {
+    public todoItems: Observable<ObservableArray<TodoItemViewModel>>;
+    constructor() {
+        this.todoItems = new Observable<ObservableArray<TodoItemViewModel>>();
+        this.todoItems.value = new ObservableArray<TodoItemViewModel>();
+        this.todoItems.value.push(new TodoItemViewModel(this, "Item 1"));
+        this.todoItems.value.push(new TodoItemViewModel(this, "Item 2"));
+        this.todoItems.value.push(new TodoItemViewModel(this, "Item 3"));
+        this.todoItems.value.push(new TodoItemViewModel(this, "Item 4"));
+    }
+}
+
+class TodoItemViewModel {
+    public name: Observable<string>;
+    public list: Observable<TodoDataModel>;
+    constructor(list: TodoDataModel, name: string) {
+        this.list = new Observable<TodoDataModel>(list);
+        this.name = new Observable<string>(name);
+    }
+}
